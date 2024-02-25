3842ea350011b4195eac5bb606d222257e08ebf1
NikKowPHP <nik.kow@outlook.com>
2024-02-25 10:10:52 +0100

    fix: Resolve issue with types placing

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card } from "react-bootstrap";
 import { formatCurrency } from "../utilities/formatCurrency";
+import { useShoppingCart } from "../context/ShoppingCartContext";
 
 type StoreItemProps = {
   id: number;
@@ -7,8 +8,9 @@ type StoreItemProps = {
   price: number;
   imgUrl: string;
 };
-export function StoreItem({ id, name, price, imgUrl }): StoreItemProps {
-  const quantity = 1;
+export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const { getItemQuantity, increaseCartQuantity, decreaseCartQuantity, removeFromCart } = useShoppingCart();
+  const quantity = getItemQuantity(id);
   return (
     <Card className="h-100">
       <Card.Img
