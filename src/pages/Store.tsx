894882dbda2753517eac04d0fe077b03720dda95
NikKowPHP <nik.kow@outlook.com>
2024-02-25 08:54:46 +0100

    feat: Incorporate store item into store page

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,14 @@
+import { Row, Col } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import { StoreItem } from "../components/StoreItem";
 export function Store() {
-    return <h1>Store</h1>
+  return (
+    <>
+      <Row md={2} xs={1} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col><StoreItem {...item} /> </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
