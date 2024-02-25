a094acd1f9f24c9525321be0f95a782f8610af11
NikKowPHP <nik.kow@outlook.com>
2024-02-25 08:16:37 +0100

    feat: Create routes within navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-
+import { Container } from "react-bootstrap";
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Store } from "./pages/Store";
+import { Navbar } from "./components/Navbar";
 function App() {
-  return <h1>dfskfjls</h1>
-
+  return (
+    <>
+    <Navbar/>
+    <Container className="mb-4">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
+    </>
+  );
 }
 
-export default App
+export default App;
