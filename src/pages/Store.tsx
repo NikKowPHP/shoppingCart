139fc6ddc20c78d87c4ac6707292af35da5eddcb
NikKowPHP <nik.kow@outlook.com>
2024-02-25 10:21:58 +0100

    add key in loop rendering

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -6,7 +6,7 @@ export function Store() {
     <>
       <Row md={2} xs={1} lg={3} className="g-3">
         {storeItems.map((item) => (
-          <Col><StoreItem {...item} /> </Col>
+          <Col key={item.id}><StoreItem {...item}  /> </Col>
         ))}
       </Row>
     </>
