e1b38c95510bd6e256f4886f3e76fcbd11cfa882
NikKowPHP <nik.kow@outlook.com>
2024-02-25 11:40:36 +0100

    feat: Render the total price of the selectedItems

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,8 @@
 import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
 import { CartItem } from "./CartItem";
+import { formatCurrency } from "../utilities/formatCurrency";
+import  storeItems  from "../data/items.json";
 type ShoppingCartProps = {
 	isOpen: boolean;
 };
@@ -12,14 +14,22 @@ export function ShoppingCart({ isOpen }: ShoppingCartProps) {
 			<Offcanvas.Header closeButton>
 				<Offcanvas.Title>Cart</Offcanvas.Title>
 			</Offcanvas.Header>
-      <Offcanvas.Body>
-        <Stack gap={3} className="d-flex align-items-center">
-          {cartItems.map(item => (
-            <CartItem key={item.id} {...item} />
-          ))}
-        </Stack>
-      </Offcanvas.Body>
+			<Offcanvas.Body>
+				<Stack gap={3} className="d-flex align-items-center">
+					{cartItems.map((item) => (
+						<CartItem key={item.id} {...item} />
+					))}
+					<div className="ms-auto fw-bold fs-5">
+						Total{" "}
+						{formatCurrency(
+							cartItems.reduce((total, cartItem) => {
+								const item = storeItems.find((i) => i.id === cartItem.id);
+								return total + (item?.price || 0) * cartItem.quantity;
+							}, 0)
+						)}
+					</div>
+				</Stack>
+			</Offcanvas.Body>
 		</Offcanvas>
 	);
-  
 }
